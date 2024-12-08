3bdb606bd30cbb2ed8e5b5fe5cc75785459aa9ca
user.mysha <user.myshaparvin@gmail.com>
2024-12-09 00:38:14 +0600

    java datatypes code files

---
+++ workspace-spring-tool-suite/javabasics/src/javadatatypes/primitivedatatypes.java
@@ -0,0 +1,16 @@
+package javadatatypes;
+
+public class primitivedatatypes {
+
+	public static void main(String[] args) {
+		int number = 16597;
+		float number2 = 15.6f;
+		double number3 = 87483748;
+		char letter = 'M';
+		boolean bool= true;
+		
+		System.out.println("Int number expressed as "+number+"\nFloat expressed as "+number2+ 
+				"\nDouble expressed as "+number3+ "\nCharacter expressed as "+letter+ "\nBoolean expressed as "+bool);
+	}
+
+}
